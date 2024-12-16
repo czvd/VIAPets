83f0bffa43ecd0f912dc885d055e9820b3c371b6
Ricky <vricsi100@gmail.com>
2024-12-16 17:37:47 +0100

    Changes

--- src/view/AddReservationViewController.java
+++ src/view/AddReservationViewController.java
@@ -337,6 +337,15 @@ public class AddReservationViewController
      alert.showAndWait();
      return;
    }
+   if (Integer.parseInt(price)<0)
+   {
+     Alert alert = new Alert(Alert.AlertType.ERROR);
+     alert.setTitle("Error");
+     alert.setHeaderText(null);
+     alert.setContentText("Price cannot be negative");
+     alert.showAndWait();
+     return;
+   }
    if(selectedCustomer==null)
    {
      Alert alert = new Alert(Alert.AlertType.ERROR);
